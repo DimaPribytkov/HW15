9ae88334e1166302fcd4e2f7817ebec5f6727be0
ДимаПрибытков <d.pribytkov93@mail.ru>
2024-02-04 17:03:02 +0300

    Печать букв алфавита в разных регистрах

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -3,7 +3,20 @@ public class Main {
     // А-123
     // Б-321...
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
+    public static String CORRECT_CHAR = " абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
     public static void main(String[] args) {
-        //todo код писать тут
+        for (int i = 1; i <CORRECT_CHAR.length(); i++) {
+            int a = CORRECT_CHAR.charAt(i);
+            System.out.println((char) a + "-" + a);
+        }
+            String i = CORRECT_CHAR.toUpperCase();
+            for (int b = 1; b <i.length(); b++){
+                int a = i.charAt(b);
+                System.out.println((char) a + "-" + a);
+            }
+
+
+
     }
 }
